e723baca279619c2ae5a2d9299fd6f7b4a215350
Tim Hannifan <hannifan@gmail.com>
2019-04-09 22:18:25 -0500

    adding tract info to crime data

--- main.py
+++ main.py
@@ -48,14 +48,19 @@ def get_df_from_socrata(client, code, whereclause=None, max_size=None):
             break
     return pd.DataFrame.from_records(combined_data)
 
-def get_census_tract(lat, lon):
+def get_tract(lat, lon):
+    print(lat, lon)
     url = 'https://geocoding.geo.census.gov/geocoder/geographies/coordinates?benchmark=Public_AR_Current&vintage=ACS2018_Current&'
     url += 'y=' + str(lat) + '&x=' + str(lon)+ '&format=json'
 
-    d = requests.get(url).json()
+
     try:
-        res = d['result']['geographies']['2010 Census Blocks'][0]['TRACT']
-        return res
+        d = requests.get(url).json()
+        parent = d['result']['geographies']['2010 Census Blocks'][0]
+        tract = parent['TRACT']
+        county = parent['COUNTY']
+        state = parent['STATE']
+        return (state, county, tract)
     except:
         print('geocode error')
         return None
@@ -70,8 +75,8 @@ def get_small_chicago_df():
     whereclause = \
         "date between '2017-01-01T00:00:00' and '2019-01-01T00:00:00'"
 
-    res = get_df_from_socrata(client, code, whereclause=whereclause, max_size=1000)
-    print('shape', res.shape)
+    res = get_df_from_socrata(client, code, whereclause=whereclause, max_size=20)
+    print('df shape', res.shape)
     return res
 
 def get_big_chicago_df():
@@ -87,5 +92,27 @@ def get_big_chicago_df():
 def test_geo_get():
     url = 'https://geocoding.geo.census.gov/geocoder/geographies/coordinates?benchmark=Public_AR_Current&vintage=ACS2018_Current&y=41.843778126&x=-87.694637678&format=json'
 
-    d = requests.get(url).json()
-    return d
+    return requests.get(url).json()
+
+def add_acs_data(df): 
+    df = df[df['latitude'].notnull()]
+    df = df[df['longitude'].notnull()]
+
+    df['tract_info'] = df.apply(lambda row: get_tract(row['latitude'],
+                                                      row['longitude']),
+                                axis=1)
+    return df
+
+def demo():
+    df = get_small_chicago_df()
+
+    augmented = add_acs_data(df)
+
+    print(augmented.iloc[0])
+    return augmented
+
+
+# https://api.census.gov/data/2017/acs/acs5/subject?get=NAME,S1901_C02_001E&for=tract:843500&in=state:17%20county:031
+
+
+# https://api.census.gov/data/2016/acs/acs5?get=NAME,B01001_001E&for=zip%20code%20tabulation%20area:60615
